5861be98b593041f8d9e0c9e4c78d0df47c99d1d
Dimitris Papatheodoulou <dimitris.papatheodoulou@kcl.ac.uk>
2017-10-10 23:36:45 +0100

    Implemented MaximumGates

--- MaximumGates.h
+++ MaximumGates.h
@@ -2,11 +2,39 @@
 #define MAXIMUM_GATES_H
 
 #include <vector>
+#include <iostream>
 using std::vector;
+using std::cout;
+using std::endl;
+
 
 // TODO: Your answer for question 2 goes here
 
-
+int maximumGates(vector<int> arrives, vector<int> departs) {
+	
+	int gatesInUse = 0;
+	int maxGates = 0;
+	
+	for (int i=1; i<arrives.size(); i++) {
+			
+		for (int j=0; j<i; j++) {
+			
+			if (arrives[i] < departs[j]) {
+				gatesInUse++;
+				std::cout << gatesInUse << endl;
+			} 
+			if (maxGates < gatesInUse) {
+				maxGates = gatesInUse;
+			}
+			
+		}
+		
+	
+	}
+	
+	return maxGates;
+	
+}
 
 
 
