5fea62e5a57117ecd3aa70c7b980fba576b4e7f8
Dimitris Papatheodoulou <dimitris.papatheodoulou@kcl.ac.uk>
2017-10-12 19:07:00 +0100

    Changed the implementation of MaximumGates

--- MaximumGates.h
+++ MaximumGates.h
@@ -3,40 +3,40 @@
 
 #include <vector>
 #include <iostream>
+#include <algorithm>
 using std::vector;
 using std::cout;
-using std::endl;
-
 
 // TODO: Your answer for question 2 goes here
 
 int maximumGates(vector<int> arrives, vector<int> departs) {
-	
+
 	int gatesInUse = 0;
 	int maxGates = 0;
-	
-	for (int i=1; i<arrives.size(); i++) {
-			
-		for (int j=0; j<i; j++) {
-			
-			if (arrives[i] < departs[j]) {
+
+	std::sort (departs.begin(), departs.end());
+	int i = 0;
+	int j = 0;
+
+	while ((i < arrives.size() && j < departs.size())) {
+
+			if (arrives[i] < departs[j-i]) {
 				gatesInUse++;
-				std::cout << gatesInUse << endl;
-			} 
+			} else {
+				gatesInUse--;
+			}
+
+			cout << "Gates in use: " << gatesInUse << "\n";
+
 			if (maxGates < gatesInUse) {
 				maxGates = gatesInUse;
 			}
-			
-		}
-		
-	
+			i++; j++;
 	}
-	
+
 	return maxGates;
-	
+
 }
-
-
 
 // Do not write any code below this line
 
