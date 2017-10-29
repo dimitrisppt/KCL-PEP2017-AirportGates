85dfc9c413c3092fa1d59fa0d0631e9a77327f73
Dimitris Papatheodoulou <dimitris.papatheodoulou@kcl.ac.uk>
2017-10-29 19:20:36 +0000

    Added the given Item.h

--- Item.h
+++ Item.h
@@ -1,8 +1,71 @@
 #ifndef ITEM_H
 #define ITEM_H
 
-// TODO: copy your Item.h file from assignment 1 over this file
+#include <iostream>
+using std::ostream;
 
+// TODO: your code for (i) goes here
+
+#include <cmath>
+
+#include <string>
+using std::string;
+
+using std::cout;
+using std::endl;
+
+class Item {
+    
+private:
+    double latitude;
+    double longitude;
+    
+    string id;
+    int secsPastHour;
+    
+public:
+    Item(const double latIn, const double longIn,
+         const string & idIn, const int secsIn) 
+        : latitude(latIn), longitude(longIn),
+          id(idIn), secsPastHour(secsIn) {            
+    }
+    
+    double distanceTo(const Item & other) const {
+        
+        static constexpr double PI = 3.14159265358979323846;
+        static constexpr double R = 6373000;
+        
+        const double lat1 = latitude * (PI / 180.0);
+        const double lon1 = longitude * (PI / 180.0);
+        
+        const double lat2 = other.latitude * (PI / 180.0);
+        const double lon2 = other.longitude * (PI / 180.0);
+        
+        const double dlon = lon2 - lon1;
+        const double dlat = lat2 - lat1;
+        const double a = pow((sin(dlat/2)), 2) + cos(lat1) * cos(lat2) * pow((sin(dlon/2)), 2);
+        const double c = 2 * atan2( sqrt(a), sqrt(1-a) );
+        const double distance = R * c;
+        
+        return distance;
+    }
+    
+    int getSecsPastHour() const {
+        return secsPastHour;
+    }
+    
+    void write(ostream & o) const {
+        o << "{" << latitude << ", " << longitude << ", \"" << id << "\", " << secsPastHour << "}";
+    }
+};
+
+ostream & operator<<(ostream & o, const Item & i) {
+    i.write(o);
+    return o;
+}
+
+
+// don't write any code below this line
 
 #endif
 
--- MapOfItems.h
+++ MapOfItems.h
@@ -3,7 +3,7 @@
 
 #include "Item.h"
 #include <vector>
-#include <cmath>
+
 
 using std::vector;
 
@@ -14,7 +14,6 @@ class MapOfItems {
 
   private:
     vector<Item> tour;
-    int tourTime = 0;
 
 
   public:
@@ -27,125 +26,7 @@ class MapOfItems {
       return tour.size();
     }
 
-    Item getItemTour(int i) {
-      return tour.at(i);
-    }
 
-
-  //   vector<Item> getTour(double walkSpeed) {
-  //     double walkTime;
-  //     double visitI;
-  //     vector<Item> visitedItems;
-  //
-  //
-  //     for (int i=0; i<size()-1; i++) {
-  //       for (int j=0; j<3600; j++) {
-  //
-  //       if (i == getItemTour(i).getSecsPastHour()) {
-  //         walkTime = getItemTour(i).distanceTo(getItemTour(i+1)) / walkSpeed;
-  //         visitI = walkTime + i;
-  //         cout << i << "\n";
-  //         cout << "\n";
-  //         cout << "Visit I: " << visitI << "\n";
-  //
-  //         if (visitI > 15 * 60) {
-  //           while ( visitI == i) {
-  //             ++visitI;
-  //
-  //           }
-  //         }
-  //         if (visitedItems[i].getSecsPastHour() > visitI) {
-  //           visitedItems.push_back(getItemTour(i));
-  //         }
-  //       }
-  //
-  //     }
-  //
-  //   }
-  //   return visitedItems;
-  // }
-
-  //   vector<Item> getTour(double walkSpeed) {
-  //     double currentTime = 0;
-  //     double walkTime = 0;
-  //     double visitI = 0;
-  //     double minVisit = 999999999;
-  //     vector<Item> tourItems;
-  //     vector<Item> tempVect = tour;
-  //
-  //     while (currentTime < 3600) {
-  //
-  //       for (int i=0; i<tour.size()-1; i++) {
-  //
-  //         for (int j=i+1; j<tour.size(); j++) {
-  //         //for (int j=i; i<j; j++) {
-  //         // cout << "Tour time: " << currentTime <<"\n";
-  //         // cout << "Element time: " << tour[i].getSecsPastHour() << "\n";
-  //         //  if (i == 0 || currentTime == tourItems[i].getSecsPastHour()) {
-  //             walkTime = tour[i].distanceTo(tour[j]) /  walkSpeed;
-  //             //cout << "WalkTime: " << walkTime << endl;
-  //             visitI = walkTime + currentTime;
-  //             //cout << "Visit I: " << visitI << endl;
-  //
-  //             if (visitI >  (15 * 60 + tour[j].getSecsPastHour())) {
-  //             //  tempVect.erase(tempVect.begin() + i);
-  //               //cout << "Too late" << "\n";
-  //               // visitI is too late, we can't visit that Item.
-  //             } else {
-  //
-  //               //cout << "Time is good" << "\n";
-  //               //cout << "VisitI: " << visitI << "\n";
-  //               //cout << "Tour i time: " << tour[i] << "\n";
-  //               if (visitI < tour[i].getSecsPastHour()) {
-  //                 //cout << "in while loop" "\n";
-  //                 visitI = tour[i].getSecsPastHour();
-  //               }
-  //
-  //
-  //
-  //               //cout << "VisitI is now: " << visitI << "\n";
-  //             }
-  //             if (visitI < minVisit) {
-  //               minVisit = visitI;
-  //               //cout << "second loop" << "\n";
-  //               tourItems.push_back(tour[i]);
-  //               currentTime = minVisit;
-  //               //cout << tourItems[i] << "\n";
-  //               //cout << currentTime << "\n";
-  //               for (int k=0; k<tourItems.size(); k++) {
-  //                 cout << tourItems[k] << "\n";
-  //               }
-  //             }
-  //             //cout << "I is : " << i << "\n";
-  //
-  //             currentTime++;
-  //
-  //           // } else {
-  //           //   ++currentTime;
-  //           // }
-  //             //cout << "J :" << j << "\n";
-  //         }
-  //
-  //
-  //
-  //
-  //         //cout << "I :" << i <<"\n";
-  //
-  //
-  //
-  //       }
-  //
-  //
-  //       //   cout << "walkTime: " << walkTime << endl;
-  //       //     cout << "WlakSpeed: " << walkSpeed << endl;
-  //             // cout << "VisitI: " << visitI << endl;
-  //               // cout << "currentTime: " <<  currentTime << endl;
-  //        //
-  //       //  cout << "VisitI: " << visitI << "\n";
-  //     }
-  //     return tourItems;
-  //
-  // }
    vector<Item> getTour(double walkSpeed) {
      double currentTime = 0;
      double walkTime = 0;
@@ -222,27 +103,7 @@ class MapOfItems {
 };
 
 
-// for (int i=0; i<tour.size()-1; i++) {
-//   for (int j=i+1; j<tour.size(); j++) {
-//     if (i == 0 || currentTime == tourItems[j].getSecsPastHour()) {
-//
-//       walkTime = tour[i].distanceTo(tour[j]) / walkSpeed;
-//       visitI = currentTime + walkTime;
-//
-//       if (visitI < (tour[i].getSecsPastHour() + 15 * 60)) {
-//         visitI = tour[i].getSecsPastHour();
-//       }
-//       if (visitI < minVisit) {
-//         minVisit = visitI;
-//         tourItems.push_back(tour[i]);
-//         currentTime = minVisit;
-//         cout << currentTime << "\n";
-//       }
-//
-//     }
-//   }
-// }
-// }
+
 
 
 
